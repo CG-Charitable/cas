252b913403fc7e23ddaae80ed71a247586846fe0
Matthias Southwick <matthias@cgcharitable.org>
2026-09-24 18:08:45 +0000

    Fix server and clients for cg

--- server.sh
+++ server.sh
@@ -1,2 +1,2 @@
-cd /home/matthias/Github/msouthwick-cas
-/home/matthias/.nvm/versions/node/v25.7.0/bin/node index.js
+cd /home/matthias/Github/cas.cgcharitable.org
+/home/matthias/.nvm/versions/node/v24.18.0/bin/node index.js
